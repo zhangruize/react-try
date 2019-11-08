3bd568355aeccb6da640de9fb1ad3fc45b8ea1ff
cdzhangruize3 <cdzhangruize3@jd.com>
2019-11-08 16:12:46 +0800

    Updates

---
+++ precache-manifest.2d0f7d011d05f8497e8408814db304c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555d64830eb2827d85d84e7c2a4bc3b1",
+    "url": "/react-try/index.html"
+  },
+  {
+    "revision": "97781e53a16cf07935ba",
+    "url": "/react-try/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "59da581b9bae7cb63b82",
+    "url": "/react-try/static/js/2.59a3bd93.chunk.js"
+  },
+  {
+    "revision": "97781e53a16cf07935ba",
+    "url": "/react-try/static/js/main.7998722e.chunk.js"
+  },
+  {
+    "revision": "31d65aa65a1cfb9abeea",
+    "url": "/react-try/static/js/runtime-main.67651554.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-try/static/media/logo.25bf045c.svg"
+  }
+]);
